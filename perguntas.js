102f43ede75fb8ad76a03f491650d75b00c15b08
geywz <00001096435871sp@al.educacao.sp.gov.br>
2024-11-28 22:43:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Portugues ',
+    'o q é substantivo?',
+    'classe gramatical de palavras que serve para nomear seres, objetos, sentimentos, ações, lugares, ideias, entre outros'
+)
+
+criaCartao(
+    'Geografia',
+    'Onde esta localizado a Africa?',
+    'sul da Europa e sudoeste da Ásia'
+)
+
+criaCartao(
+    'Filosofia',
+    'Quem foi Diogenes?',
+    'Diógenes, o Cínico, foi um filósofo da Grécia Antiga'
+)
+
+criaCartao(
+    'Artes',
+    'O que é cubismo ',
+    'movimento artístico que surgiu na França no início do século XX, rompendo com os padrões artísticos e culturais da época'
+)
